9d6417264d1e7d0f3552a34d3f27639ac7d6e542
Robin Hughes <robin22391@hotmail.com>
2019-01-13 22:32:35 +0000

    initial commit

---
+++ client/src/views/select_view.js
@@ -0,0 +1,31 @@
+import PubSub from '../helpers/pub_sub/js';
+
+class SelectView (element) {
+  constructor(container) {
+     this.element = element;
+   }
+};
+
+bindEvents() {
+  PubSub.subscribe('InstrumentFamilies:data-ready', (evt) => {
+    const allInstrumentFamilies = evt.detail;
+    this.populate(allInstrumentFamilies);
+  });
+};
+
+  addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+populate(instrumentFamilyData) {
+  instrumentFamilyData.forEach((familiy, index) => {
+    const option = document.createElement('option');
+    option.textContent = familiy.name;
+    option.value = index;
+    this.element.appendChild(option);
+  });
+};
+
+export default InstrumentFamilyView;
